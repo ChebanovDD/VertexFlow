faf86d3cd8b0684287e3b7b872c0b679075d1778
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2021-09-28 12:12:40 +0700

    Added revit addin.

---
+++ src/VertexFlow.RevitAddin/App.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.UI;
+
+namespace VertexFlow.RevitAddin
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            TaskDialog.Show("VertexFlow", $"OnStartup.");
+            return Result.Succeeded;
+        }
+        
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            TaskDialog.Show("VertexFlow", $"OnShutdown.");
+            return Result.Succeeded;
+        }
+    }
+}
